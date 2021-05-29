5b17d99f05ef09a5c92b3f9afa12078f2f978ed8
Nebojsa Kamber <kamber@nebojsa.dev>
2021-05-29 23:09:36 +0200

    Add Static Dumper and test it's working

--- config/bundles.php
+++ config/bundles.php
@@ -4,4 +4,5 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symplify\SymfonyStaticDumper\SymfonyStaticDumperBundle::class => ['all' => true],
 ];
