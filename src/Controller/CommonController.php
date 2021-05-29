7bba487d8358cbd35dcfd338e5262d5a29f7d886
Nebojsa Kamber <kamber@nebojsa.dev>
2021-05-30 00:47:15 +0200

    Add About Us Add fonts

--- src/Controller/CommonController.php
+++ src/Controller/CommonController.php
@@ -6,11 +6,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class IndexController extends AbstractController
+class CommonController extends AbstractController
 	{
 	#[Route('/', methods: ['GET'])]
-	public function foo(): Response
+	public function index(): Response
 		{
 		return $this->render('index.html.twig');
+		}
+
+	#[Route('/o-nama', methods: ['GET'])]
+	public function aboutUs(): Response
+		{
+		return $this->render('about_us.html.twig');
 		}
 	}
