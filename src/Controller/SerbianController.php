1be10d7c39cfee36ef25c600f2188135e9a2ba02
Nebojsa Kamber <kamber@nebojsa.dev>
2021-06-01 01:35:18 +0200

    Add sortiments (SR)

--- src/Controller/SerbianController.php
+++ src/Controller/SerbianController.php
@@ -38,6 +38,72 @@ class SerbianController extends AbstractController
 		return $this->render('serbian/apple_indigenous_varieties.html.twig');
 		}
 
+	#[Route('/kruska', methods: ['GET'])]
+	public function pear(): Response
+		{
+		return $this->render('serbian/pear.html.twig');
+		}
+
+	#[Route('/dunja-musmula', methods: ['GET'])]
+	public function quinceMedlar(): Response
+		{
+		return $this->render('serbian/quince_medlar.html.twig');
+		}
+
+	#[Route('/breskva-nektarina', methods: ['GET'])]
+	public function peachNectarine(): Response
+		{
+		return $this->render('serbian/peach_nectarine.html.twig');
+		}
+
+	#[Route('/kajsija', methods: ['GET'])]
+	public function apricot(): Response
+		{
+		return $this->render('serbian/apricot.html.twig');
+		}
+
+	#[Route('/sljiva', methods: ['GET'])]
+	public function plum(): Response
+		{
+		return $this->render('serbian/plum.html.twig');
+		}
+
+	#[Route('/tresnja-visnja', methods: ['GET'])]
+	public function cherrySourCherry(): Response
+		{
+		return $this->render('serbian/cherry_sour_cherry.html.twig');
+		}
+
+	#[Route('/orah-leska-badem', methods: ['GET'])]
+	public function walnutHazelnutAlmond(): Response
+		{
+		return $this->render('serbian/walnut_hazelnut_almond.html.twig');
+		}
+
+	#[Route('/dren', methods: ['GET'])]
+	public function cornel(): Response
+		{
+		return $this->render('serbian/cornel.html.twig');
+		}
+
+	#[Route('/ribizla-josta-ogrozd', methods: ['GET'])]
+	public function currantGooseBerryJost(): Response
+		{
+		return $this->render('serbian/currant_gooseberry_jost.html.twig');
+		}
+
+	#[Route('/dud', methods: ['GET'])]
+	public function mulberry(): Response
+		{
+		return $this->render('serbian/mulberry.html.twig');
+		}
+
+	#[Route('/stubaste-patuljaste-forme', methods: ['GET'])]
+	public function columnarAndDwarfForms(): Response
+		{
+		return $this->render('serbian/columnar_dwarf_forms.html.twig');
+		}
+
 	#[Route('/sadnja', methods: ['GET'])]
 	public function planting(): Response
 		{
